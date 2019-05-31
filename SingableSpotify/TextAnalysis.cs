ae847fb17daec5e51f8bfab4a0fb99069f2e4340
John Singh <wethegreenpeople@gmail.com>
2019-05-30 23:22:10 -0700

    Add TextAnalysis class

---
+++ SingableSpotify/TextAnalysis.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingableSpotify
+{
+    public class TextAnalysis
+    {
+        public TextAnalysis()
+        {
+
+        }
+
+        public int FleschReadingEase(string text)
+        {
+            return 0;
+        }
+    }
+}
