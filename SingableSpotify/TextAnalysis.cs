b82e8a844be529ec1b20b2219c74a075006afb00
John <wethegreenpeople@gmail.com>
2019-05-31 16:55:05 -0700

    Working on syllable counting

--- SingableSpotify/TextAnalysis.cs
+++ SingableSpotify/TextAnalysis.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SingableSpotify
@@ -14,6 +15,44 @@ namespace SingableSpotify
         public int FleschReadingEase(string text)
         {
             return 0;
+        }
+
+        private int WordCount(string text)
+        {
+            return text.Split(' ').Length;
+        }
+
+        private int SentenceCount(string text)
+        {
+            return text.Split('.', '?', '!', ';').Length;
+        }
+
+        private int SyllableCount(string text)
+        {
+            text = text.ToLower();
+            var vowels = new[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+            int syllableCount = 0;
+            bool wasPreviousCharacterVowel = false;
+
+            foreach (var word in text.Split(' '))
+            {
+                foreach (var character in text)
+                {
+                    if (vowels.Contains(character))
+                    {
+                        if (!wasPreviousCharacterVowel) { ++syllableCount; }
+                        wasPreviousCharacterVowel = true;
+                    }
+                    else
+                    {
+                        wasPreviousCharacterVowel = false;
+                    }
+                }
+
+                if ((word.EndsWith("e") || (word.EndsWith("es") || word.EndsWith("ed")))
+                      && !word.EndsWith("le"))
+                    syllableCount--;
+            }
         }
     }
 }
